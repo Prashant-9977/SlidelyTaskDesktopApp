3bd24ca522904433849fb2ed492d0816a2b4e41f
Prashant-9977 <132604649+Prashant-9977@users.noreply.github.com>
2024-06-21 00:18:31 +0530

    Update CreateSubmissionForm.Designer.vb

--- SlidelyTask/CreateSubmissionForm.Designer.vb
+++ SlidelyTask/CreateSubmissionForm.Designer.vb
@@ -14,12 +14,10 @@ Partial Class CreateSubmissionForm
         End Try
     End Sub
 
-    'Required by the Windows Form Designer
+    
     Private components As System.ComponentModel.IContainer
 
-    'NOTE: The following procedure is required by the Windows Form Designer
-    'It can be modified using the Windows Form Designer.  
-    'Do not modify it using the code editor.
+    
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.Label1 = New System.Windows.Forms.Label()
